07d8d7174bc86f1d8c316cc067d388a0b7a7f8e6
Shreyas Kumar <shreyaskumar467@gmail.com>
2024-02-25 08:56:08 +0530

    splash update gig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation ("de.hdodenhof:circleimageview:3.1.0") //Circular Image
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28") //Animated Gif
 
 }
